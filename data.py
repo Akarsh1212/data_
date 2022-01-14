21e128298fb965b08587be2204c931d192518bb6
Akarsh1212 <70149526+Akarsh1212@users.noreply.github.com>
2022-01-14 16:53:48 +0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,46 @@
+import plotly.figure_factory as ff
+import csv
+import plotly.graph_objects as go
+import statistics
+import random
+import pandas as pd
+
+df = pd.read_csv("data.csv")
+
+data = df["temp"].tolist()
+def random_set_of_mean (counter):
+    dataset = []
+    for i in range(0 , counter):
+        random_index = random.randint(0,len(data)-1)
+        value = data[random_index]
+        dataset.append(value)
+    
+    mean = statistics.mean(dataset)
+    return mean
+def show_fig ( mean_list ):
+    df = mean_list
+    mean = statistics.mean(df)
+    fig = ff.create_distplot([df],["temp"],show_hist = False)
+    fig.add_trace(go.Scatter(x = [mean, mean],y = [0,1],mode = "lines" , name = "MEAN"))
+    fig.show()
+def setup ():
+    mean_list = []
+    for i in range(0,1000):
+        set_of_mean = random_set_of_mean(100)
+        mean_list.append(set_of_mean)
+    show_fig(mean_list)
+    mean = statistics.mean(mean_list)
+    print("Mean of sampling distribution : ",mean)
+
+setup()
+
+population = statistics.mean(data)
+print("Population mean :- " , population)
+def stdev ():
+    mean_list = []
+    for i in range(0,1000):
+        set_of_mean = random_set_of_mean(100)
+        mean_list.append(set_of_mean)
+    stdev = statistics.stdev(mean_list)
+    print("Stander deviation : ",stdev)
+stdev()
